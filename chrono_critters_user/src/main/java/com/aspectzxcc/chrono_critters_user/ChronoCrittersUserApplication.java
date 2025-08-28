3ebf7c3922255358c23ff04654e521a66828acc7
aspectzxcc <jos10nathan@gmail.com>
2025-08-29 03:14:06 +0800

    renamed user service

--- chrono_critters_user/src/main/java/com/aspectzxcc/chrono_critters_user/ChronoCrittersUserApplication.java
+++ chrono_critters_user/src/main/java/com/aspectzxcc/chrono_critters_user/ChronoCrittersUserApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChronoCrittersApplication {
+public class ChronoCrittersUserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChronoCrittersApplication.class, args);
+		SpringApplication.run(ChronoCrittersUserApplication.class, args);
 	}
 
 }
