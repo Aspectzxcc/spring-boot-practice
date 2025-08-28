7f2a2e861a9914177e59e4bab4e9f7cfda7068c8
aspectzxcc <jos10nathan@gmail.com>
2025-08-28 22:30:07 +0800

    spring boot init

---
+++ chrono-critters/src/test/java/com/aspectzxcc/chrono_critters/ChronoCrittersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aspectzxcc.chrono_critters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronoCrittersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
