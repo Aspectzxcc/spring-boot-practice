9e7fa5f5677d24e32ece4baca752a5aac36f1804
aspectzxcc <jos10nathan@gmail.com>
2025-08-29 03:01:03 +0800

    added lobby service

---
+++ chrono_critters_lobby/src/main/java/com/chronocritters/chrono_critters_lobby/ChronoCrittersLobbyApplication.java
@@ -0,0 +1,13 @@
+package com.chronocritters.chrono_critters_lobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronoCrittersLobbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronoCrittersLobbyApplication.class, args);
+	}
+
+}
---
+++ chrono_critters_lobby/src/test/java/com/chronocritters/chrono_critters_lobby/ChronoCrittersLobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chronocritters.chrono_critters_lobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronoCrittersLobbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
