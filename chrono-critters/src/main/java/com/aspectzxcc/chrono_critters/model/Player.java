f72721fa6bafcf8294e1ff36ad659339a19987e8
aspectzxcc <jos10nathan@gmail.com>
2025-08-28 23:22:34 +0800

    added player as db model

--- chrono-critters/src/main/java/com/aspectzxcc/chrono_critters/model/Player.java
+++ chrono-critters/src/main/java/com/aspectzxcc/chrono_critters/model/Player.java
@@ -1,9 +1,13 @@
 package com.aspectzxcc.chrono_critters.model;
 
 import java.util.List;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.Data;
 
 @Data
+@Document(collection = "players")
 public class Player {
     private String id;
     private String username;
