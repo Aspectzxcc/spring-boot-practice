791563226547f639552b2891295f928f7797b573
aspectzxcc <jos10nathan@gmail.com>
2025-08-29 04:29:00 +0800

    added disconnect listener unimplemented

---
+++ chrono_critters_lobby/src/main/java/com/aspectzxcc/chrono_critters_lobby/listener/WebSocketEventListener.java
@@ -0,0 +1,13 @@
+package com.aspectzxcc.chrono_critters_lobby.listener;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+public class WebSocketEventListener {
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+    }
+}
