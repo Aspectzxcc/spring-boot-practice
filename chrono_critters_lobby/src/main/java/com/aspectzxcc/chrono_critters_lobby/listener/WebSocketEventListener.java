cc4ddcd88cbb78bf76add24462b51ebb39129eae
aspectzxcc <jos10nathan@gmail.com>
2025-08-29 04:34:01 +0800

    added very simple disconnection handling

--- chrono_critters_lobby/src/main/java/com/aspectzxcc/chrono_critters_lobby/listener/WebSocketEventListener.java
+++ chrono_critters_lobby/src/main/java/com/aspectzxcc/chrono_critters_lobby/listener/WebSocketEventListener.java
@@ -1,13 +1,25 @@
 package com.aspectzxcc.chrono_critters_lobby.listener;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import com.aspectzxcc.chrono_critters_lobby.model.LobbyEvent;
 
 @Component
 public class WebSocketEventListener {
 
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        LobbyEvent disconnectEvent = new LobbyEvent();
+        disconnectEvent.setUsername("Anonymous");
+        disconnectEvent.setMessage("Left the lobby");
+
+        simpMessagingTemplate.convertAndSend("/topic/lobby", disconnectEvent);
     }
 }
