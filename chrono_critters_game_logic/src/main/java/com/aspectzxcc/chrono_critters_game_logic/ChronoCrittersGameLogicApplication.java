554b38f7fd7ebb0097c31030664d67f38de2722d
aspectzxcc <jos10nathan@gmail.com>
2025-08-29 17:39:01 +0800

    game logic service init

---
+++ chrono_critters_game_logic/src/main/java/com/aspectzxcc/chrono_critters_game_logic/ChronoCrittersGameLogicApplication.java
@@ -0,0 +1,13 @@
+package com.aspectzxcc.chrono_critters_game_logic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronoCrittersGameLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronoCrittersGameLogicApplication.class, args);
+	}
+
+}
