95b658b094f5c22862b6ca241e858d2ec7c8113f
aspectzxcc <jos10nathan@gmail.com>
2025-08-30 18:50:56 +0800

    added test frontend and cors to user service

--- chrono_critters_parent/chrono_critters_user/src/main/java/com/aspectzxcc/chrono_critters_user/ChronoCrittersUserApplication.java
+++ chrono_critters_parent/chrono_critters_user/src/main/java/com/aspectzxcc/chrono_critters_user/ChronoCrittersUserApplication.java
@@ -2,6 +2,9 @@ package com.aspectzxcc.chrono_critters_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ChronoCrittersUserApplication {
@@ -10,4 +13,13 @@ public class ChronoCrittersUserApplication {
 		SpringApplication.run(ChronoCrittersUserApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/graphql").allowedOrigins("*");
+			}
+		};
+	}
 }
